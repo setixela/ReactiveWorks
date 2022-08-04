c87e9377d90f798ab2113c60e624010b2667b74e
AlexSolo <setixela@gmail.com>
2022-08-04 23:19:27 +0300

    w

---
+++ Sources/ReactiveWorks/Stateable/Stateable.swift
@@ -0,0 +1,89 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aleksandr Solovyev on 04.08.2022.
+//
+
+import Foundation
+
+// MARK: - Stateable
+
+public protocol Stateable: InitProtocol {
+   associatedtype State
+
+   func applyState(_ state: State)
+}
+
+public extension Stateable {
+   init(_ states: State...) {
+      self.init(states)
+   }
+
+   init(_ states: [State]) {
+      self.init()
+
+      states.forEach { applyState($0) }
+   }
+
+   @discardableResult
+   func set(_ state: State) -> Self {
+      applyState(state)
+
+      return self
+   }
+
+   @discardableResult
+   func set(_ states: [State]) -> Self {
+      states.forEach { applyState($0) }
+
+      return self
+   }
+
+   @discardableResult
+   func set(_ states: State...) -> Self {
+      states.forEach { applyState($0) }
+
+      return self
+   }
+}
+
+public protocol Stateable2: Stateable {
+   associatedtype State2
+
+   func applyState(_ state: State2)
+}
+
+public extension Stateable2 {
+   @discardableResult
+   func set(_ state: State2) -> Self {
+      applyState(state)
+
+      return self
+   }
+
+   @discardableResult
+   func set(_ state: [State2]) -> Self {
+      state.forEach { applyState($0) }
+
+      return self
+   }
+}
+
+// protocol Stateable3: Stateable2 {
+//    associatedtype State3
+//
+//    func applyState(_ state: State3)
+// }
+//
+// protocol Stateable4: Stateable3 {
+//    associatedtype State4
+//
+//    func applyState(_ state: State4)
+// }
+//
+// protocol Stateable5: Stateable4 {
+//    associatedtype State5
+//
+//    func applyState(_ state: State5)
+// }

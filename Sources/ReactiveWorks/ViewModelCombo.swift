70a89211f89e39ee7c87f6c35f01fefcd577aa15
AlexSolo <setixela@gmail.com>
2022-08-06 19:14:29 +0300

    wmCombo
    fatal
    
    
    w
    
    
    w

--- Sources/ReactiveWorks/ViewModelCombo.swift
+++ Sources/ReactiveWorks/ViewModelCombo.swift
@@ -1,8 +1,87 @@
+////
+////  File.swift
+////  
+////
+////  Created by Aleksandr Solovyev on 06.08.2022.
+////
 //
-//  File.swift
-//  
+//import UIKit
 //
-//  Created by Aleksandr Solovyev on 06.08.2022.
+//public protocol Combo {
+////   var mainModel: Self { get }
+//}
 //
-
-import Foundation
+//public extension Combo {
+//   var mainModel: Self { self }
+//}
+//
+//public protocol ComboRight: Combo {
+//
+//}
+//
+//public protocol ComboLeft: Combo {
+//
+//}
+//
+//public protocol ComboLeftRight: ComboLeft, ComboRight {
+//
+//}
+//
+//public protocol LeftExtender: Combo {
+//   associatedtype LeftModel: ViewModelProtocol
+//
+//   var leftModel: LeftModel { get }
+//}
+//
+//public extension LeftExtender {
+//   func set(leftModel: (LeftModel) -> Void) -> Self {
+//      leftModel(self.leftModel)
+//      return self
+//   }
+//}
+//
+//public protocol RightExtender: Combo {
+//   associatedtype RightModel: ViewModelProtocol
+//
+//   var rightModel: RightModel { get }
+//}
+//
+//public extension RightExtender {
+//   func set(rightModel: (RightModel) -> Void) -> Self {
+//      rightModel(self.rightModel)
+//      return self
+//   }
+//}
+//
+//public extension ViewModelProtocol where Self: LeftExtender {
+//   var uiView: UIView {
+//      print("uiview")
+//      let stackView = UIStackView()
+//      stackView.addArrangedSubview(self.leftModel.uiView)
+//      stackView.addArrangedSubview(self.uiView)
+//      return stackView
+//   }
+//}
+//
+////
+//public extension ViewModelProtocol where Self: RightExtender {
+//   var uiView: UIView {
+//      print("uiview")
+//      let stackView = UIStackView()
+//      stackView.addArrangedSubview(self.rightModel.uiView)
+//      stackView.addArrangedSubview(self.uiView)
+//      return stackView
+//   }
+//}
+//
+////
+//public extension ViewModelProtocol where Self: RightExtender, Self: LeftExtender {
+//   var uiView: UIView {
+//      print("uiview")
+//      let stackView = UIStackView()
+//      stackView.addArrangedSubview(self.leftModel.uiView)
+//      stackView.addArrangedSubview(self.uiView)
+//      stackView.addArrangedSubview(self.rightModel.uiView)
+//      return stackView
+//   }
+//}

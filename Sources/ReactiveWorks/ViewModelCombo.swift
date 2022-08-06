957ddd82b86eaa5003a7cdd28bc0e5ba03a25e17
AlexSolo <setixela@gmail.com>
2022-08-06 20:34:25 +0300

    w

--- Sources/ReactiveWorks/ViewModelCombo.swift
+++ Sources/ReactiveWorks/ViewModelCombo.swift
@@ -7,32 +7,32 @@
 
 import UIKit
 
-public protocol Combo {}
-
-public extension Combo {
-   var mainModel: Self { self }
-}
-
-public protocol ComboRight: Combo {
-   associatedtype RightModel: ViewModelProtocol
-
-   var rightModel: RightModel { get }
-}
-
-public extension ComboRight {
-   func setRight(_ closure: (RightModel) -> Void) -> Self {
-      closure(rightModel)
-      return self
-   }
-}
-
-public extension UIViewModel where Self: ComboRight {
-   var uiView: UIView {
-      print("uiview")
-      let stackView = UIStackView()
-      stackView.axis = .horizontal
-      stackView.addArrangedSubview(uiView)
-      stackView.addArrangedSubview(rightModel.uiView)
-      return stackView
-   }
-}
+//public protocol Combo {}
+//
+//public extension Combo {
+//   var mainModel: Self { self }
+//}
+//
+//public protocol ComboRight: Combo {
+//   associatedtype RightModel: ViewModelProtocol
+//
+//   var rightModel: RightModel { get }
+//}
+//
+//public extension ComboRight {
+//   func setRight(_ closure: (RightModel) -> Void) -> Self {
+//      closure(rightModel)
+//      return self
+//   }
+//}
+//
+//public extension UIViewModel where Self: ComboRight {
+//   var uiView: UIView {
+//      print("uiview")
+//      let stackView = UIStackView()
+//      stackView.axis = .horizontal
+//      stackView.addArrangedSubview(uiView)
+//      stackView.addArrangedSubview(rightModel.uiView)
+//      return stackView
+//   }
+//}

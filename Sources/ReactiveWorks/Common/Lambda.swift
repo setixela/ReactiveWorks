b53effe88fdb4fdb06ee76174208a7bed675db56
AlexSolo <setixela@gmail.com>
2022-09-04 22:35:25 +0300

    wip

--- Sources/ReactiveWorks/Common/Lambda.swift
+++ Sources/ReactiveWorks/Common/Lambda.swift
@@ -43,7 +43,6 @@ public struct Lambda<T>: LambdaProtocol where T: Any {
       guard let value = value as? T else {
          print("Lambda payloads not conform: {\(value.self)} is not {\(T.self)}")
          fatalError()
-         return
       }
 
       lambda(value)
